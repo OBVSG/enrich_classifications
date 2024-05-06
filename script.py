4def8f23f30179854997076b91dcf52c4e6aaf85
Philipp Kozjak <philipp.kozjak@obvsg.at>
2024-05-06 15:03:58 +0200

    black linting

--- script.py
+++ script.py
@@ -9,9 +9,7 @@ parser = ArgumentParser(
 parser.add_argument(
     "input_xml", type=Path, help="File containing the MARCXML Data. No namespaces!"
 )
-parser.add_argument(
-    "output_xml", type=Path, help="File to write the output."
-)
+parser.add_argument("output_xml", type=Path, help="File to write the output.")
 parser.add_argument(
     "ddc_bk_csv", type=Path, help="File to with csv mappping table ddc bk"
 )
@@ -27,7 +25,9 @@ ddc_obv_map = enrich_cls.create_notation_map(args.ddc_obv_csv)
 result_collection = ET.Element("collection")
 
 for record in tree.getroot():
-    new_bib = enrich_cls.enrich_bib(record, ddc_to_bk=ddc_bk_map, ddc_to_obv=ddc_obv_map)
+    new_bib = enrich_cls.enrich_bib(
+        record, ddc_to_bk=ddc_bk_map, ddc_to_obv=ddc_obv_map
+    )
     if new_bib:
         result_collection.append(new_bib)
 
