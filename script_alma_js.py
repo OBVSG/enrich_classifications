4ba23e9c099017e88217baca46a30086cf8f4472
Philipp Kozjak <philipp.kozjak@obvsg.at>
2024-05-28 12:05:52 +0200

    use concordance id for api requests, use requests.Session, call new functions

--- script_alma_js.py
+++ script_alma_js.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser
 from os.path import basename
 import glob
+import requests
 import xml.etree.ElementTree as ET
 import enrich_cls
+
 
 parser = ArgumentParser(
     description="Enrich BK and OBV Fachgruppe for Marc Records on Basis of DDC"
@@ -13,18 +15,28 @@ parser.add_argument(
     help="Input directory containing xmls of alma bib api objects",
 )
 parser.add_argument(
-    "output_xml_dir", type=str, help="Output directory to write enriched marc xml files to"
+    "output_xml_dir",
+    type=str,
+    help="Output directory to write enriched marc xml files to",
 )
 
 args = parser.parse_args()
 
-DDC_BK_URL = "https://coli-conc.gbv.de/api/mappings?partOf=https%3A%2F%2Fcoli-conc.gbv.de%2Fapi%2Fconcordances%2Fddc-bk-1000-ubg&download=csv"
-DDC_OBV_URL = "https://coli-conc.gbv.de/api/mappings?partOf=https%3A%2F%2Fcoli-conc.gbv.de%2Fapi%2Fconcordances%2Fddc-obv&download=csv"
+DDC_BK_ID = "ddc-bk-1000-ubg"
+DDC_OBV_ID = "ddc-obv"
 
-ddc_bk_map = enrich_cls.create_notation_map_via_download(DDC_BK_URL)
-ddc_obv_map = enrich_cls.create_notation_map_via_download(DDC_OBV_URL)
 
-print(f"Downloaded {len(ddc_bk_map)} mappings for BK and {len(ddc_obv_map)} mappings for OBV")
+with requests.Session() as s:
+    ddc_bk_concordance = enrich_cls.download_concordance(s, DDC_BK_ID)
+    ddc_obv_concordance = enrich_cls.download_concordance(s, DDC_OBV_ID)
+
+
+ddc_bk_map = enrich_cls.create_notation_map(ddc_bk_concordance)
+ddc_obv_map = enrich_cls.create_notation_map(ddc_obv_concordance)
+
+print(
+    f"Downloaded {len(ddc_bk_map)} mappings for BK and {len(ddc_obv_map)} mappings for OBV"
+)
 
 counter = 0
 for xml_file in glob.glob(f"{args.input_xml_dir}*.xml"):
